f822e427b2e0c510f78f5304824e4e7c558084d1
KVK Siddartha <sidduultralegend@gmail.com>
2023-10-19 04:11:17 +0530

    more frontend

--- meowmeow/src/components/Home.js
+++ meowmeow/src/components/Home.js
@@ -9,10 +9,10 @@ export default function Home() {
     <div>
         <div className="intro">
             <p id="WelcomeText">Welcome to</p>
-            <p id="meowtitle">Meow meow store</p>
-                <div style={{position: "relative", flex: "0 0 45%", transform: "translate3d(35%, 10%, 0)", width: "50% ", height: "auto"}}><img class="img-fluid" data-bss-parallax="" data-bss-parallax-speed="0.8" src={pewds}/></div>
-                <div style={{position: "relative",flex: "0 0 45%",transform: "translate3d(170%, -50%, 0)", width: "30%", height: "auto"}}><img class="img-fluid" data-bss-parallax="" data-bss-parallax-speed="0.4" src={alia}/></div>
-                <div style={{position: "relative",flex: "0 0 60%",transform: "translate3d(-20%, -60%, 0)"}}><img class="img-fluid" data-bss-parallax="" data-bss-parallax-speed="0.25" src={gucci}/></div>
+            <p id="meowtitle"></p>
+            <div style={{position: "relative", flex: "0 0 45%", transform: "translate3d(35%, 10%, 0)", width: "50% ", height: "auto"}}><img class="img-fluid" data-bss-parallax="" data-bss-parallax-speed="0.8" src={pewds}/></div>
+            <div style={{position: "relative",flex: "0 0 45%",transform: "translate3d(170%, -50%, 0)", width: "30%", height: "auto"}}><img class="img-fluid" data-bss-parallax="" data-bss-parallax-speed="0.4" src={alia}/></div>
+            <div style={{position: "relative",flex: "0 0 60%",transform: "translate3d(-20%, -60%, 0)"}}><img class="img-fluid" data-bss-parallax="" data-bss-parallax-speed="0.25" src={gucci}/></div>
         </div>
     </div>
   )
